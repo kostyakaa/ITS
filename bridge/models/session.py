f608047597b2fbdb2341f48cb367c5c9a439e25d
kostyaka <kosty-karmanov@mail.ru>
2025-11-05 22:40:24 +0300

    попытка исправить пролаги

--- bridge/models/session.py
+++ bridge/models/session.py
@@ -111,21 +111,7 @@ class Session:
         try:
             while True:
                 msg = await self.ws.receive_text()
-                try:
-                    data = json.loads(msg)
-                except json.JSONDecodeError:
-                    continue
-
-                if data.get("type") != "control":
-                    continue
-
-                cmd = str(data.get("cmd", "")).strip()
-                value = data.get("value")
-
-                if value is None or value == "":
-                    line = f"{cmd}\n"
-                else:
-                    line = f"{cmd} {value}\n"
+                break
 
                 try:
                     writer.write(line.encode("utf-8"))
